964c5f90cc69f1584bab2ffad6058f4511817f09
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-24 17:08:53 -0300

    Inicio do projeto

---
+++ SomaImparConsec/SomaImparConsec/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SomaImparConsec {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite o primeiro valor:");
+            int X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor:");
+            int y = int.Parse(Console.ReadLine());
+            int maiorNumero = 0;
+            int menorNumero = 0;
+
+            if (X > y) {
+                maiorNumero = X;
+                menorNumero = y;
+            }
+            else {
+                maiorNumero = y;
+                menorNumero = X;
+            }
+            int soma = 0;
+            for (int i = menorNumero + 1; i < maiorNumero; i++) {
+                if (i % 2 != 0) {
+                    soma = i + soma;
+                }
+            }
+            Console.WriteLine("Resultado: " + soma);
+        }
+    }
+}
